616f6a31c7a86e46298183e487ff9519b86e2387
Zehra AKBULUT <zehraakbulut333@gmail.com>
2023-05-03 01:07:31 +0300

    ders 14 sonu

--- Edukator.BusinessLayer/Concrete/CourseManager.cs
+++ Edukator.BusinessLayer/Concrete/CourseManager.cs
@@ -18,6 +18,11 @@ namespace Edukator.BusinessLayer.Concrete
             _courseDal = courseDal;
         }
 
+        public List<Course> TGetCourseWithCategory()
+        {
+            return _courseDal.GetCoursesWithCategory();
+        }
+
         public void TDelete(Course t)
         {
             _courseDal.Delete(t);
