b24ede403ab48da593545ea31cefefde47eca9c1
Zehra AKBULUT <zehraakbulut333@gmail.com>
2023-06-22 15:16:00 +0300

    22.06.2023

---
+++ Edukator.PresentationLayer/ViewComponents/Course/_CourseFeaturedPartial.cs
@@ -0,0 +1,25 @@
+﻿using Edukator.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edukator.PresentationLayer.ViewComponents.Course
+{
+    public class _CourseFeaturedPartial : ViewComponent
+    {
+        private readonly ICourseService _courseService;
+
+        public _CourseFeaturedPartial(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _courseService.TGetCourseWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ Edukator.PresentationLayer/ViewComponents/Course/_CourseFindsPartial.cs
@@ -0,0 +1,26 @@
+﻿using Edukator.BusinessLayer.Abstract;
+using Edukator.DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edukator.PresentationLayer.ViewComponents.Course
+{
+    public class _CourseFindsPartial : ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public _CourseFindsPartial(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutService.TGetList();
+            return View(values);
+        }
+    }
+}
